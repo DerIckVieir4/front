985cd254ce82fd36eb92489537473c0873b0298d
DerickSV <carlosvieira535@gmail.com>
2022-10-10 21:27:55 -0300

    atualizando agora

--- src/pages/cadastroEmpresa/index.js
+++ src/pages/cadastroEmpresa/index.js
@@ -14,7 +14,6 @@ export default function CadastroEmpresa() {
     const [dados,setDados]=useState([]);
     const [responsavel,setResponsavel] = useState("");
     
-
     useEffect(()=>{
         mostrardados();
     },[])
